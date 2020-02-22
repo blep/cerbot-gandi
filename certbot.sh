c90851c58e7169fcac90c7d778748501c612a3c4
Baptiste Lepilleur <baptiste.lepilleur@gmail.com>
2020-02-22 08:33:55 +0100

    disabled shell debug mode to avoid writing API key to stdout.

--- certbot.sh
+++ certbot.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-set -ex
+set -e
 echo Writing GANDI_API_KEY environment variable to /app/gandi.ini
 echo certbot_plugin_gandi:dns_api_key=$GANDI_API_KEY > /app/gandi.ini
 chmod 600 /app/gandi.ini
